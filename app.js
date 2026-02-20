949bcd462998343e88b02a098696b0e685b2869a
Isokhon Xabibullayev <isoxonhabibullayev6@gmail.com>
2026-02-20 15:27:23 +0500

    Practice lesson 2

---
+++ app.js
@@ -0,0 +1,26 @@
+// Lesson 2
+
+
+//Practice
+console.log("5" + 5)
+console.log("5" - 5)
+console.log("5" * "2")
+console.log("10" / 2)
+console.log(5 + true)
+console.log("5" + true)
+console.log("5" - true)
+console.log("5" + null)
+console.log("5" + undefined)
+console.log("5" - null)
+console.log("5" - undefined)
+
+let num = "200"
+console.log(Number(num) + 50)
+console.log(String(25) + " Years old")
+console.log(Boolean("false"))
+console.log("10" - true)
+
+
+
+console.log(a)
+var a = 10
