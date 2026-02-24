1c0fff52fe72d50064907095a4fd57f0e2b7e45b
UlugbekYusupov <bilmadim.uz@gmail.com>
2026-02-24 17:40:25 +0500

    lecture 2

---
+++ app.js
@@ -0,0 +1,18 @@
+// var a = undefined;
+
+// console.log(a);
+// let a = 20;
+
+// greet();
+
+// function greet() {
+//   console.log("hello");
+// }
+
+// console.log(Array())
+
+// Array.prototype.greet = function () {
+//   console.log("Arrayy prototypedan keyapdi");
+// };
+
+// Array().greet();
