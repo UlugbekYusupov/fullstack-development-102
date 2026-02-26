a637b66dc336064d2741fc150bbd0fd7cdf13578
Abdulloh <ablyusupov17@gmail.com>
2026-02-26 16:54:46 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,104 @@
+// let num = 10;
+// let odd = [];
+// let even = [];
+
+// for(let i = 1; i < num; i++) {
+//     if(i % 2 === 0) {
+//         even.push(i);
+//     } else {
+//         odd.push(i)
+//     }
+// }
+
+// console.log(odd, odd.length);
+// console.log(even, even.length);
+
+
+// let num = 4567;
+// const digits = []
+
+// while (num > 0) {
+//     digits.push(num % 10)
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(digits.join(''));
+
+
+// let n = parseInt(prompt("Enter a number:"));
+
+// let even = 0;
+// let odd = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 === 0) {
+//         even++;
+//     } else {
+//         odd++;
+//     }
+// }
+
+// console.log("Even numbers:", even);
+// console.log("Odd numbers:", odd);
+
+
+// let num = parseInt(prompt("Enter a number:"));
+
+// let reverse = 0;
+
+// while (num > 0) {
+//     let digit = num % 10;      
+//     reverse = reverse * 10 + digit;
+//     num = Math.floor(num / 10);
+// }
+
+// console.log("Reversed number:", reverse);
+
+
+// let num = parseInt(prompt("Enter a number:"));
+
+// let largest = 0;
+
+// while (num > 0) {
+//     let digit = num % 10;
+
+//     if (digit > largest) {
+//         largest = digit;
+//     }
+
+//     num = Math.floor(num / 10);
+// }
+
+// console.log("Largest digit:", largest);
+
+// let num = parseInt(prompt("Enter a number:"));
+
+// let sumDigits = 0;
+
+// while (num > 0) {
+//     let digit = num % 10;
+//     sumDigits += digit;
+//     num = Math.floor(num / 10);
+// }
+
+// console.log("Sum of digits:", sumDigits);
+
+
+// let num = parseInt(prompt("Enter a number:"));
+
+// let count = 0;
+
+// if (num === 0) {
+//     count = 1;
+// } else {
+//     while (num > 0) {
+//         num = Math.floor(num / 10);
+//         count++;
+//     }
+// }
+
+// console.log("Number of digits:", count);
+
+
+
+
