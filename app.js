905abe30882c9d27038b146943348bf2715efd37
Saidakbar-Axrorov <saidakbaraxrorov10@gmail.com>
2026-02-26 17:57:56 +0500

    Comment out code for exercises in app.js
    Comment out various exercises and logic in app.js

--- app.js
+++ app.js
@@ -1,23 +1,94 @@
-// var a = undefined;
 
-// console.log(a);
-// let a = 20;
+// exercise 1
 
-// greet();
-
-// function greet() {
-//   console.log("hello");
+// for loop logic
+// let number = Number(prompt("Raqam kirirting"))
+// let evencount = 0
+// let oddcount = 0
+// for (let i= 0; i<number; i++) {
+//     if (i%2==0) {
+//         evencount++
+//     }
+//     else {
+//         oddcount++
+//     }
 // }
-
-// console.log(Array())
-
-// Array.prototype.greet = function () {
-//   console.log("Arrayy prototypedan keyapdi");
-// };
-
-// Array().greet();
+// console.log(evencount);
+// console.log(oddcount + " lol");
 
 
-function add () {
-    
-}
+// while loop logic
+
+// let number = Number(prompt("Raqam kirirting"))
+// let evencount = 0
+// let oddcount = 0
+// let i=0
+// while (i<number) {
+//     if (i%2==0) {
+//         evencount++
+//     }
+//     else {
+//         oddcount++
+//         }
+//     i++
+// }
+// console.log("Juft sonlar soni: ", evencount);
+// console.log( "Toq sonlar soni" ,oddcount);
+
+
+
+// exercise2
+
+// let number = Number(prompt("Enter a number"));
+// let reversed = 0;
+// while (number > 0) {
+//     let digit = number % 10;
+//     reversed = reversed * 10 + digit;
+//     number = (number-digit)/10
+// }
+// console.log(reversed);
+
+
+
+// exercise3
+
+// let a = Number(prompt("Enter a number: "));
+// a = Math.abs(a);
+// let katta = 0;
+// let raqam = 0;
+
+// while (a > 0) {
+//     raqam = a % 10;
+//     a= Math.floor(a/10)
+//     if (raqam>katta) {
+//         katta=raqam;
+//     }
+// }
+// console.log(katta);
+
+
+
+// exercise4
+
+// let a = Number(prompt("Enter a number: "));
+// a = Math.abs(a);
+// let raqam = 0;
+// let sum=0;
+// while (a>0) {
+//     raqam=a%10;
+//     a= Math.floor(a/10)
+//     sum+=raqam
+// }
+// console.log(sum);
+
+
+// exercise5
+
+// let a = Number(prompt("Enter a number: "));
+// raqam=0;
+// let count=0;
+// while (a>0){
+//     a= Math.floor(a/10)
+//     count++
+// }
+// console.log(count);
