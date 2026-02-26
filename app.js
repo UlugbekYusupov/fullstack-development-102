8b36f15a3d5d66409f738bab03dbb6a619f6bbff
Reality <Reality-uz>
2026-02-26 14:25:56 +0500

    ...

--- app.js
+++ app.js
@@ -1,49 +1,114 @@
-// 1
-console.log("5" + 5);       // "55" string 
-console.log("5" - 5);       // 0
-console.log("5" * "2");     // 10
-console.log("10" / 2);      // 5
-console.log(5 + true);      // 6
-console.log("10" - true);   // 9
-console.log("5" + true);    // "5true"
-console.log(5 + null);      // 5
-console.log(5 + undefined); // NaN
+// // 1
+// console.log("5" + 5);       // "55" string 
+// console.log("5" - 5);       // 0
+// console.log("5" * "2");     // 10
+// console.log("10" / 2);      // 5
+// console.log(5 + true);      // 6
+// console.log("10" - true);   // 9
+// console.log("5" + true);    // "5true"
+// console.log(5 + null);      // 5
+// console.log(5 + undefined); // NaN
 
-// 2
-const num1 = Number("200") + 50;  //alternative one
-console.log(num1);       // Output: 250
+// // 2
+// const num1 = Number("200") + 50;  //alternative one
+// console.log(num1);       // Output: 250
 
-const age = 25;
-const num2 = age.toString() + " years old";
-console.log(num2);      // Output: 25 years old
+// const age = 25;
+// const num2 = age.toString() + " years old";
+// console.log(num2);      // Output: 25 years old
 
-const stringFalse = "false";
-const num3 = Boolean(stringFalse);
-console.log(num3);      // Output: true
+// const stringFalse = "false";
+// const num3 = Boolean(stringFalse);
+// console.log(num3);      // Output: true
 
-console.log("10" - true); // Output: 9 // Because "10" is turned to 10 and true is 1, so 10 - 1 = 9
+// console.log("10" - true); // Output: 9 // Because "10" is turned to 10 and true is 1, so 10 - 1 = 9
 
-// 3
-const cel = Number(prompt("Enter temp in Celsius: "));
+// // 3
+// const cel = Number(prompt("Enter temp in Celsius: "));
 
-const fah = (cel * 9 / 5) + 32;
+// const fah = (cel * 9 / 5) + 32;
 
-console.log(`Celsius: ${cel}°C`);
-console.log(`Fahrenheit: ${fah}°F`);
-// What happens if change the const value: JS shows error, because Const cant be reassigned
+// console.log(`Celsius: ${cel}°C`);
+// console.log(`Fahrenheit: ${fah}°F`);
+// // What happens if change the const value: JS shows error, because Const cant be reassigned
 
-// 4
-let totalBudget = Number(prompt("Enter your total budget: "));
+// // 4
+// let totalBudget = Number(prompt("Enter your total budget: "));
 
-const expense1 = Number(prompt("Enter expense 1: "));
-const expense2 = Number(prompt("Enter expense 2: "));
-const expense3 = Number(prompt("Enter expense 3: "));
+// const expense1 = Number(prompt("Enter expense 1: "));
+// const expense2 = Number(prompt("Enter expense 2: "));
+// const expense3 = Number(prompt("Enter expense 3: "));
 
-const Expenses = expense1 + expense2 + expense3;
-const remainingBudget = totalBudget - Expenses;
+// const Expenses = expense1 + expense2 + expense3;
+// const remainingBudget = totalBudget - Expenses;
 
-if (remainingBudget < 0){
-    console.warn("Warning: You have overspent");
+// if (remainingBudget < 0){
+//     console.warn("Warning: You have overspent");
+// }
+// // Bonus:
+// console.log(`Remaining Budget: $${remainingBudget.toFixed(2)}`);
+
+
+
+//24.02.2026. Classwork: Challenges 1-5
+//1
+let n=prompt("Enter number:");
+let even = 0;
+let odd = 0;
+
+for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+        even++;
+    } else {
+        odd++;
+    }
 }
-// Bonus:
-console.log(`Remaining Budget: $${remainingBudget.toFixed(2)}`);
+console.log("Even numbers count: " + even);
+console.log("Odd numbers count: " + odd);
+
+//2
+let num=prompt("Enter a number:");
+let result = 0;
+
+while (num > 0) {
+    let lastDigit = num % 10;
+    result = result * 10 + lastDigit;
+    num = Math.floor(num / 10);
+}
+console.log("Result: " + result);
+
+//3
+let number =prompt("Enter a number:");
+let largest = 0;
+
+while (number > 0){
+    let son = number % 10;
+    if (son>largest) { largest = son; }
+    number = Math.floor(number / 10);
+}
+console.log("Largest digit: " + largest);
+
+//4
+let nums= prompt("Enter a number:");
+let sum = 0;
+
+while (nums > 0){
+    let lastDigit= nums%10;
+    sum = sum+lastDigit;
+    nums= Math.floor(nums/10);
+}
+console.log("Sum of digits: " + sum);
+
+//5
+let numbers= prompt("Enter a number:");
+let count = 0;
+
+if (numbers===0){
+    count = 1;
+} else {
+    while (numbers > 0){
+        numbers = Math.floor(numbers / 10);
+        count++;
+    }
+}
+console.log("Total digits: " + count);
