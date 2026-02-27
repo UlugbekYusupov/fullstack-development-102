23999f1065e8ab5f41ad62b8f19bdb27fe7bd235
mxasanova785-arch <mxasanova785@gmail.com>
2026-02-27 13:17:35 +0500

    Control Statements homework

--- app.js
+++ app.js
@@ -81,3 +81,107 @@ function challenge5(n) {
 }
 
 challenge5(98765);
+
+
+//homework 
+// controle statements 
+// TASK 1 — Largest of two numbers
+
+let a = 10;
+let b = 25;
+
+if (a > b) {
+    console.log("Task 1: Largest number is " + a);
+} else {
+    console.log("Task 1: Largest number is " + b);
+}
+
+// TASK 2 — Find the sign of 3 numbers
+let x = 3;
+let y = -7;
+let z = 2;
+
+if (x * y * z > 0) {
+    console.log("Task 2: The sign is +");
+} else {
+    console.log("Task 2: The sign is -");
+}
+
+// TASK 3 — Sort three numbers
+let n1 = 0;
+let n2 = -1;
+let n3 = 4;
+
+if (n1 >= n2 && n1 >= n3) {
+    if (n2 >= n3) console.log("Task 3:", n1, n2, n3);
+    else console.log("Task 3:", n1, n3, n2);
+}
+else if (n2 >= n1 && n2 >= n3) {
+    if (n1 >= n3) console.log("Task 3:", n2, n1, n3);
+    else console.log("Task 3:", n2, n3, n1);
+}
+else {
+    if (n1 >= n2) console.log("Task 3:", n3, n1, n2);
+    else console.log("Task 3:", n3, n2, n1);
+}
+
+// TASK 4 — Odd or Even (0–15)
+for (let i = 0; i <= 15; i++) {
+    if (i % 2 === 0) {
+        console.log("Task 4:", i + " is even");
+    } else {
+        console.log("Task 4:", i + " is odd");
+    }
+}
+
+// TASK 5 — FizzBuzz (1–100)
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("Task 5: FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log("Task 5: Fizz");
+    }
+    else if (i % 5 === 0) {
+        console.log("Task 5: Buzz");
+    }
+    else {
+        console.log("Task 5:", i);
+    }
+}
+// TASK 6 — Armstrong Numbers
+for (let num = 100; num <= 999; num++) {
+
+    let temp = num;
+    let sum = 0;
+
+    while (temp > 0) {
+        let digit = temp % 10;
+        sum += digit ** 3;
+        temp = Math.floor(temp / 10);
+    }
+
+    if (sum === num) {
+        console.log("Task 6: Armstrong number =", num);
+    }
+}
+// TASK 7 — Average Marks & Grade
+let marks = [80, 77, 88, 95, 68];
+let total = 0;
+
+for (let i = 0; i < marks.length; i++) {
+    total += marks[i];
+}
+
+let average = total / marks.length;
+let grade;
+
+if (average < 60) grade = "F";
+else if (average < 70) grade = "D";
+else if (average < 80) grade = "C";
+else if (average < 90) grade = "B";
+else grade = "A";
+
+console.log("Task 7: Average =", average);
+console.log("Task 7: Grade =", grade);
