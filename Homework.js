c3a00c124248baf94d0511a5a8fd1d587544e97c
Reality <Reality-uz>
2026-02-27 13:56:53 +0500

    ...

--- Homework.js
+++ Homework.js
@@ -195,3 +195,26 @@ for (let i= 100; i<= 999; i++) {
     }
     if (sum===i){ console.log(i); }
 }
+
+//7
+let students = [
+    { name: "David",  marks: 80 },
+    { name: "Vinoth", marks: 77 },
+    { name: "Divya",  marks: 88 },
+    { name: "Ishitha",marks: 95 },
+    { name: "Thomas", marks: 68 }
+];
+
+let total= 0;
+for (let i= 0; i< students.length; i++) { total += students[i].marks; }
+
+let average = total / students.length;
+console.log("Average Marks: " + average);
+
+let grade;
+if (average < 60) { grade= "F"; }
+else if (average <70) { grade= "D"; }
+else if (average <80) { grade= "C"; }
+else if (average <90) { grade= "B"; }
+else if (average<100) { grade= "A"; }
+console.log("Grade: " + grade);
