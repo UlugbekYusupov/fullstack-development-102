9bf54bae2ef6b840811b8183cf656bc99dd44df9
UlugbekYusupov <bilmadim.uz@gmail.com>
2026-02-27 17:03:23 +0500

    lecture 4

--- app.js
+++ app.js
@@ -1,18 +1,76 @@
-// var a = undefined;
+// // var a = undefined;
 
-// console.log(a);
-// let a = 20;
+// // console.log(a);
+// // let a = 20;
 
-// greet();
+// // greet();
 
-// function greet() {
-//   console.log("hello");
-// }
+// // function greet() {
+// //   console.log("hello");
+// // }
 
-// console.log(Array())
+// // console.log(Array())
 
-// Array.prototype.greet = function () {
-//   console.log("Arrayy prototypedan keyapdi");
+// // Array.prototype.greet = function () {
+// //   console.log("Arrayy prototypedan keyapdi");
+// // };
+
+// // Array().greet();
+
+// console.log(5 === "5");
+
+// console.log("Ulugbek" === "Ulugbek");
+
+// const user = {
+//   name: "Ulugbek",
 // };
 
-// Array().greet();
+// const userr = user;
+
+// console.log(user || userr);
+
+// let number = prompt();
+
+// for (let i = 1; i < number; i++) {
+
+// }
+
+// let number = prompt("Enter a number : ")
+
+// i = 0
+
+// while(number > 0){
+
+//   let x = number % 10
+
+// }
+
+// for (let i = 0; i < 10; i++) {}
+
+// let i = 0;
+// while (i < 10) {
+//   i++;
+// }
+
+// do {
+//   i++;
+// } while (i < 10);
+
+// const user = {
+//   name: "Ulugbek",
+//   age: 20,
+//   country: "Uzbekistan",
+// };
+
+// let keys = Object.keys(user);
+
+// for (const key in user) {
+//   const element = user[key];
+//   console.log(element);
+// }
+
+// for (const element of keys) {
+//   console.log(element);
+// }
+
+// keys.forEach(function (element) {});
