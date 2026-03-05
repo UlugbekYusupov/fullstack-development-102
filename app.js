ea806a8714e4c4d5a41fd016f34464c52d18445d
Abdulloh <ablyusupov17@gmail.com>
2026-03-05 16:38:08 +0500

    updated

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // let num = 10;
 // let odd = [];
 // let even = [];
@@ -115,7 +116,6 @@
 //     alert("The signed number is: " + num3);
 // }
 
-
 // for (let i = 1; i <= 15; i++) {
 //     if (i % 2 === 0) {
 //         console.log(i + " is even");
@@ -135,3 +135,80 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+// // var a = undefined;
+
+// // console.log(a);
+// // let a = 20;
+
+// // greet();
+
+// // function greet() {
+// //   console.log("hello");
+// // }
+
+// // console.log(Array())
+
+// // Array.prototype.greet = function () {
+// //   console.log("Arrayy prototypedan keyapdi");
+// // };
+
+// // Array().greet();
+
+// console.log(5 === "5");
+
+// console.log("Ulugbek" === "Ulugbek");
+
+// const user = {
+//   name: "Ulugbek",
+// };
+
+// const userr = user;
+
+// console.log(user || userr);
+
+// let number = prompt();
+
+// for (let i = 1; i < number; i++) {
+
+// }
+
+// let number = prompt("Enter a number : ")
+
+// i = 0
+
+// while(number > 0){
+
+//   let x = number % 10
+
+// }
+
+// for (let i = 0; i < 10; i++) {}
+
+// let i = 0;
+// while (i < 10) {
+//   i++;
+// }
+
+// do {
+//   i++;
+// } while (i < 10);
+
+// const user = {
+//   name: "Ulugbek",
+//   age: 20,
+//   country: "Uzbekistan",
+// };
+
+// let keys = Object.keys(user);
+
+// for (const key in user) {
+//   const element = user[key];
+//   console.log(element);
+// }
+
+// for (const element of keys) {
+//   console.log(element);
+// }
+
+// keys.forEach(function (element) {});
+
