1b24f11b70613da234a20f0878a66d9992697b9b
abilovashirin <abiljonovao7@gmail.com>
2026-03-05 09:58:18 +0500

    homework

---
+++ app.js
@@ -0,0 +1,27 @@
+// #1
+console.log("5" + 5)
+console.log("5" - 5)
+console.log("5" * 2)
+console.log("10" / 2)
+console.log(5 + true)
+console.log("10" - true)
+console.log("5" + true)
+console.log(5 + null)
+console.log(5 + undefined)
+
+// #2
+console.log(200 + 50)
+console.log(25 + "years old")
+console.log(Boolean("false"))
+console.log("10" - true)
+
+// #3
+const celsius = 30
+const fahrenheit = (celsius * 9/5) + 32
+console.log("Celsius:", celsius)
+console.log("Fahrenheit:", fahrenheit)
+
+// const celsius = Number(prompt("Enter temperature in Celsius:"));
+// const fahrenheit = (celsius * 9 / 5) + 32;
+// console.log(`Celsius: celsius`);
+// console.log(`Fahrenheit: fahrenheit`);
