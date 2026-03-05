775dbea06c2eebcf0a0554f5c3344eedfc2435bb
Muhammadqodir <moydinovmuhammadqodir47@gmail.com>
2026-03-05 18:18:50 +0500

    fixed

---
+++ dars 1/main.js
@@ -0,0 +1,77 @@
+// var global  | let o'zgaruvchi | const o'zgarmas
+
+// var l = 3;
+// {
+//     var l = 4;
+    
+// }
+// console.log(l)
+
+
+// let login = "wolf";
+// let password = "1234"
+
+
+// let day = 7;
+// switch (day) {
+//     case 1:
+//         console.log("Dushanba");
+//         break;
+//     case 2:
+//         console.log("Seshanba");
+//         break;
+//     case 3:
+//         console.log("Chorshanba");
+//         break;
+//     case 4:
+//         console.log("Payshanba");
+//         break;
+//     case 5:
+//         console.log("Juma");
+//         break;
+//     case 6:
+//         console.log("Shanba");
+//         break;
+//     case 7:
+//         console.log("Yakshanba");
+//         break;
+//     default:
+//         console.log("Noto'g'ri raqam kiritildi!");
+// }
+
+
+let oy = 2;
+switch (oy) {
+  case 1:
+    case 2:
+        case 12:
+       {
+    console.log("qish fasli");
+    break;
+  }
+   case 3:
+   case 4:
+   case 5:
+        {
+    console.log("bahor fasli");
+    break;
+  }
+   case 6:
+   case 7:
+   case 8:
+   {
+    console.log("yoz fasli");
+    break;
+  }
+   case 9:
+   case 10:
+   case 11:
+   {
+    console.log("kuz fasli");
+    break;
+  }
+  default: {
+    console.log("bunday oy mavjud emas");
+  }
+
+}
