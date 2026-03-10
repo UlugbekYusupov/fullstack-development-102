0f04669392d8f4bcb76aecebeda87d0e30d353df
Abdulloh-Mannopov <abdullohmannopov111@gmail.com>
2026-03-10 07:34:23 +0500

    Math operations

---
+++ Training/app.js
@@ -0,0 +1,14 @@
+let myAge = 18
+let humanDogRatio = 7
+let myDogAge = myAge*humanDogRatio;
+console.log(myDogAge);
+
+bonusPoints = 50
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints);
+bonusPoints = bonusPoints - 70
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints);
+
