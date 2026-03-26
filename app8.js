1433288de79c2df29fdef5f97eda10dbfd021b80
Saidakbar-Axrorov <saidakbaraxrorov10@gmail.com>
2026-03-26 17:37:31 +0500

    fixed

---
+++ app8.js
@@ -0,0 +1,45 @@
+// let car = {
+//     brand: 'tesla',
+//     model: "model s",
+//     year: 2024,
+//     start: function (){
+//         console.log("Car is starting...");
+//     }
+// };
+// console.log(car.model);
+// car.start();
+
+// let person = new Object();
+// person.name = 'Alice';
+// person.age = 25;
+// person.greet = function () {
+// return `Hello, my name is ${this.name}`;
+// }
+// console.log(person.greet());
+
+// function createPerson(name, age){
+//     return {
+//         name,
+//         age,
+//         greet() {
+//             return `hi, I am ${this.name}, and i am ${this.age} years old.`
+//         }
+//     }
+// }
+// let person1= createPerson("Alice", 18);
+// let person2= createPerson("Bob", 99);
+// console.log(person1.greet());
+// console.log(person2.greet());
+
+// function Person(name, age){
+// this.name = name;
+// this.age = age;
+// this.greet = function() {
+//     return `hi, I am ${this.name}, and i am ${this.age} years old.`
+//     };
+// }
+// let person1= new Person("Alice", 18);
+// let person2= new Person("Bob", 99);
+// console.log(person1.greet());
+// console.log(person2.greet());
+
---
+++ domlearning.js
@@ -0,0 +1,119 @@
+// const title = document.getElementById('main-heading');
+// console.log(title);
+
+// const listItems = document.getElementsByClassName('list-items')
+// console.log(listItems);
+ 
+// const listItems = document.getElementsByTagName('li')
+// console.log(listItems);
+
+// const container = document.querySelector('div');
+// console.log(container);
+
+
+// const title = document. querySelector('#main-heading');
+// title.style.color = 'blue'; 
+// console.log(title);
+
+// const listItems = document.querySelectorAll('.list-items')
+// for (i=0; i<listItems.length; i++){
+//     listItems[i].style.fontSize = '1.5rem';
+// }
+
+// const firstlistitem = document.querySelector('.list-items')
+// console.log(firstlistitem.innerText);
+// console.log(firstlistitem.textContent);
+// console.log(firstlistitem.innerHTML);
+
+// const ul = document.querySelector('ul');   
+// const li = document.createElement('li');
+// ul.append(li);
+
+// li.innerText = 'X-men';
+
+// li.setAttribute('class', 'list-items');
+// li.removeAttribute('class')
+
+// li.classList.add('list-items') 
+// // li.classList.remove('list-items')
+// console.log(li.classList.contains('list-items'));
+
+// li.remove();
+
+// const title = document.querySelector('#main-heading');
+// console. log(title.getAttribute('id'));
+
+// Parent Node Traversal
+// let ul = document.querySelector('ul');
+// // console.log(ul.parentNode.parentNode);
+// // console.log(ul.parentElement.parentElement);
+// const html = document. documentElement;
+// console.log(html.parentNode);
+
+// let ul = document.querySelector('ul');
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+// ul.childNodes[1].style.backgroundColor = 'blue'; 
+// console.log(ul.children);
+// console.log(ul.firstElementChild);
+// console.log(ul.lastElementChild);
+
+// Sibling Node Traversal
+// let ul = document.querySelector('ul');
+// const div = document.querySelector('div');
+// console.log(div.childNodes);
+// console.log(ul.previousElementSibling);
+// console.log(ul.nextElementSibling);
+
+
+// Event Listeners
+// element.addEventListener("click", function);
+
+// const buttonTwo = document.querySelector('.btn-2');
+
+// function alertbtn(){
+//     alert('I also really hate Javascript')
+// }
+// buttonTwo.addEventListener('click', alertbtn)
+
+// const newbackgroundcolor = document.querySelector('.box-3');
+// function changebgcolor () {
+//     newbackgroundcolor.style.backgroundColor = 'navy';
+
+// };
+// newbackgroundcolor.addEventListener('mouseover', changebgcolor)
+
+
+
+// const revealbtn = document.querySelector('.reveal-btn');
+// const hiddenContent = document.querySelector('.hidden-content');
+// function revealContent(){
+//     if(hiddenContent.classList.contains('reveal-btn')){
+//         hiddenContent.classList.remove('reveal-btn')
+//     } else {hiddenContent.classList.add('reveal-btn')}
+// };
+// revealbtn.addEventListener('click', revealContent)
+
+
+
+// window.addEventListener("click", function() {
+//     console.log('Window');
+// }, true);
+
+// document.addEventListener("click", function() {
+//     console.log('Document');
+// }, true);
+
+// document.querySelector(".div2").addEventListener("click", function() {
+//     console.log('DIV 2');
+// }, true);
+
+// document.querySelector(".div1").addEventListener("click", function() {
+//     console.log('DIV 1');
+// }, true);
+
+// document.querySelector("button").addEventListener("click", function(e) {
+//     console.log(e.target.innerText = 'clicked!');
+//     ;
+// }, true);
