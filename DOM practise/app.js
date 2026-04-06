9deadc1aaeecda567ef2720b43d39f52de804ede
Muhammadqodir <moydinovmuhammadqodir47@gmail.com>
2026-04-06 10:03:30 +0500

    Muhammadqodir

---
+++ DOM practise/app.js
@@ -0,0 +1,9 @@
+const title = document.getElementById(`main-heading`);
+console.log(title);
+
+
+// const list = document.getElementsByClassName(`list-items`)
+// console.log(list)
+
+const list = document.getElementsByTagName(`li`)
+console.log(list)
